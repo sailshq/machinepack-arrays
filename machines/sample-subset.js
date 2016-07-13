df9662a4cc1c72e4f2fafc456fc7a55f296c1e7f
Scott Gress <scottmgress@gmail.com>
2016-07-13 11:03:32 -0500

    Add "pick random subset" machine

---
+++ machines/sample-subset.js
@@ -0,0 +1,68 @@
+module.exports = {
+
+
+  friendlyName: 'Pick random subset',
+
+
+  description: 'Randomly select an unordered subset of the array',
+
+
+  extendedDescription: 'Note that even if "Subset size" is 1, an array will still be returned (as opposed to the Pick random item" machine which always returns a single value)',
+
+
+  cacheable: false,
+
+
+  sync: true,
+
+
+  inputs: {
+
+    array: {
+      friendlyName: 'Array',
+      example: ['*'],
+      description: 'The array of items to pick from.',
+      required: true
+    },
+
+    size: {
+      friendlyName: 'Subset size',
+      example: 2,
+      description: 'The number of items to pick out of the array',
+      required: true
+    }
+
+  },
+
+
+  exits: {
+
+    error: {
+      description: 'Unexpected error occurred.'
+    },
+
+    emptyArray: {
+      friendlyName: 'Empty array',
+      description: 'The provided array has no items.'
+    },
+
+    success: {
+      description: 'Returns a randomly selected unordered subset of the array.',
+      like: 'array',
+      outputFriendlyName: 'Random subset',
+      outputDescription: 'A random subset selected from the array.'
+
+    }
+
+  },
+
+
+  fn: function(inputs, exits) {
+    var _ = require('lodash');
+    if (inputs.array.length === 0) {
+      return exits.emptyArray();
+    }
+    return exits.success(_.sample(inputs.array, inputs.size));
+  }
+
+};
