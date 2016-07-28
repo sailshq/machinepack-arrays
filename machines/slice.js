2e44dc02ce4c1a98932a1e18e007ae97e104584a
Scott Gress <scottmgress@gmail.com>
2016-07-28 15:42:02 -0500

    Use `error` exit in `slice` machine when invalid starting or ending index inputs are specified

--- machines/slice.js
+++ machines/slice.js
@@ -24,7 +24,7 @@ module.exports = {
     start: {
       friendlyName: 'Start from (index)',
       description: 'The index of the first item to include in the new array.',
-      extendedDescription: 'This index should be zero or a positive number.',
+      extendedDescription: 'Must be a non-negative integer.',
       example: 2,
       required: true
     },
@@ -32,7 +32,7 @@ module.exports = {
     end: {
       friendlyName: 'End with (index)',
       description: 'The index of the last item to include in the new array.',
-      extendedDescription: 'This index should be zero or a positive number. Omitting this input value will include the rest of the array.',
+      extendedDescription: 'Must be a non-negative integer.  Omitting this input value will include the rest of the array.',
       example: 5
     }
 
@@ -52,19 +52,35 @@ module.exports = {
 
 
   fn: function (inputs,exits) {
+
+    // Import `lodash`.
     var _ = require('lodash');
 
-    if (inputs.start < 0) {
-      return exits.error('`start` index must be least zero.');
+
+    // If an invalid start index is given, return through the `error` exit.
+    if (inputs.start < 0 || Math.floor(inputs.start) !== inputs.start) {
+      return exits.error('Starting index must be a non-negative integer.');
     }
+
+    // If no ending index is give, return the rest of the array (beginning
+    // with the start index) through the `success` exit.
     if (typeof inputs.end === 'undefined') {
       return exits.success(_.slice(inputs.array, inputs.start));
     }
 
-    if (inputs.end < 0) {
-      return exits.error('`end` index must be least zero.');
+    // If an invalid end index is given, return through the `error` exit.
+    if (inputs.end < 0 || Math.floor(inputs.end) !== inputs.end) {
+      return exits.error('Ending index must be a non-negative integer.');
     }
-    // Increment `end` by 1 (since the third arg to `_.slice` is exclusive)
+
+    // If the end index is less than the beginning, return through the `error` exit.
+    if (inputs.end < inputs.start) {
+      return exits.error('Ending index must >= starting index.');
+    }
+
+    // Increment `end` by 1 (since the third arg to `_.slice` is exclusive),
+    // and return the result of the Lodash `_.slice()` function through the
+    // `success` exit.
     return exits.success(_.slice(inputs.array, inputs.start, inputs.end+1));
   },
 
