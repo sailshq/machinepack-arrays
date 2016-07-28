9b4d221a5eaeec2fb8dcce696d2700c94ac30f66
Scott Gress <scottmgress@gmail.com>
2016-07-28 15:38:02 -0500

    Update `pluck` getExample to use `_.pluck` and RTTC to send back an accurate exemplar.

--- machines/pluck.js
+++ machines/pluck.js
@@ -47,17 +47,19 @@ module.exports = {
         }
 
         // If the name of the key is not available yet, we can't use it to figure out which
-        // item to grab, so the best we can do is send back ['*']
+        // item to grab, so the best we can do is send back ['*'].
         if (_.isUndefined(inputs.key)) {
           return ['*'];
         }
 
         // But if we have the key name, and the array is available with at least one item,
-        // we can just borrow that first item, grab the value of the `key`, and if that exists,
-        // use that to build our example.
+        // we can run _.pluck() on the array and use RTTC to get an exemplar from the result.
+        // Note the use of "strict" mode (the last argument to coerceExemplar) to ensure that
+        // the arrays like [1,'1'] are coerced to ['*'] and not ['1']
         if (!_.isUndefined(inputs.array[0][inputs.key])) {
-          return [inputs.array[0][inputs.key]];
+          return env.rttc.coerceExemplar(_.pluck(inputs.array, inputs.key), false, false, true);
         }
+
         // Otherwise, if it doesn't exist, we'll fall back to the same generic guarantee we
         // used above.
         return ['*'];
