ef2498559b3000033751e89bf9efb03856c00638
Scott Gress <scottmgress@gmail.com>
2016-07-28 15:40:50 -0500

    Use `error` exit in `replace` machine when invalid index input is specified

--- machines/replace.js
+++ machines/replace.js
@@ -24,6 +24,7 @@ module.exports = {
     index: {
       friendlyName: 'Replace at (index)',
       description: 'The index of the item to replace.',
+      extendedDescription: 'Must be a non-negative integer.',
       example: 1,
       required: true
     },
@@ -47,6 +48,11 @@ module.exports = {
 
     },
 
+    invalidIndex: {
+      description: 'The specified index was not a non-negative integer.'
+    },
+
+
     notFound: {
       friendlyName: 'Index out of bounds',
       description: 'The array does not have enough items for anything to exist at the specified index.',
@@ -57,16 +63,23 @@ module.exports = {
 
   fn: function (inputs,exits) {
 
-    if (inputs.index < 0) {
-      return exits.error('Index must be least zero.');
+    // If an invalid index is given, return through the `error` exit.
+    if (inputs.index < 0 || Math.floor(inputs.index) !== inputs.index) {
+      return exits.error('Index must be a non-negative integer.');
     }
+
+    // If the array is too small to accomodate the specified index, return
+    // through the `notFound` exit.  Note that it is acceptable to specify
+    // an index at the _end_ of the array; for example an index of 4 for
+    // an array with length 3.
     if (inputs.array.length < inputs.index) {
       return exits.notFound();
     }
 
-    // Replace item
+    // Replace the item in the array.
     inputs.array.splice(inputs.index, 1, inputs.value);
 
+    // Return the resulting array through the `success` exit.
     return exits.success(inputs.array);
 
   },
